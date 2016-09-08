4583614cfc7e5fa1a7e1a7ec056b8cf3d07bb625
Marco Perone <m.perone@mvlabs.it>
2016-09-08 13:12:56 +0200

    correct typos and remove commented code

--- src/src/App/Service/MiddlewareService.php
+++ src/src/App/Service/MiddlewareService.php
@@ -44,7 +44,7 @@ class MiddlewareService
         // Wait on all of the requests to complete. Throws a ConnectException if any of the requests fail
         try {
             $results = Promise\unwrap($requests);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             return false;
         }
 
@@ -57,34 +57,6 @@ class MiddlewareService
 
         return $this->data;
     }
-
-    /**
-     * @return array
-     */
-    /*public function getMiddlewaresTest() //test with async non-concurrent requests
-    {
-        $retData = $this->data; // is this right?
-        $parsedResponses = [];
-
-        foreach ($retData as $mi) {
-
-            $arr = explode ( '/' , $mi->packagistUrl );
-            $promise = $this->client->getAsync('https://packagist.org/packages/' . $arr[4] . "/" . $arr[5] . ".json");
-            $promise->then(
-                function (ResponseInterface $res){
-                    $parsedResponse = json_decode($res->getBody());
-                    $mi->stars = strval($parsedResponse->package->github_stars);
-                    $mi->downloads = strval($parsedResponse->package->downloads->total);
-                    echo "done";
-                },
-                function (RequestException $e){
-                    echo $e->getMessage() . "\n";
-                }
-            );
-            $promise->wait();
-        }
-        return array_values($retData);
-    }*/
 
     /**
      * @param string $middlewareSlug name of the slug
@@ -110,33 +82,4 @@ class MiddlewareService
 
         return false;
     }
-
-    /**
-     * @param string $middlewareSlug
-     * @return mixed returns the middleware on success, false on failure
-     */
-    /*public function getMiddlewareTest($middlewareSlug) //test with file_get_content
-    {
-        foreach ($this->data as $middleware) {
-            if ($middleware->slug === $middlewareSlug) {
-
-                //find request parameters from packagistUrl
-                $arr = explode ( '/' , $middleware->packagistUrl ); //4 author - 5 packageName
-
-                //send a GET request
-                $response = file_get_contents("https://packagist.org/packages/" . $arr[4] . "/" . $arr[5] . ".json");
-
-                //decode the JSON response
-                $parsedResponse = json_decode($response);
-
-                //update stars and DL values before returning
-                $middleware->stars = strval($parsedResponse->package->github_stars);
-                $middleware->downloads = strval($parsedResponse->package->downloads->total);
-
-                return $middleware;
-            }
-        }
-
-        return false;
-    }*/
 }
